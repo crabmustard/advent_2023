a65adedf747e0c479983a76bff59fd9d0022ee0c
crabmustard <bryce.lippert@gmail.com>
2023-12-04 06:28:14 -0500

    cleaned up a function

--- dayfour/day4two.py
+++ dayfour/day4two.py
@@ -1,46 +1,43 @@
-import re
 
 
 def run_tests(file):
-    reward_options = {} 
-    card_count = {}
+    rewards = {} 
+    cards = {}
     final_rewards = {}
     with open(file, 'r') as f:
         while walrus := f.readline():
-            game, rewards = check_card(walrus)
-            reward_options[game] = rewards
-            card_count[game] = 1
+            game, reward = check_card(walrus)
+            rewards[game] = reward
+            cards[game] = 1
         
-    final_rewards = add_to_ralf(card_count, reward_options)
+    final_rewards = check_dem_thingers(cards, rewards)
     final_count = final_tally(final_rewards)
     return final_count
 
-def add_to_ralf(ticket_count, rewards):
-    rw = rewards.copy()
+def check_dem_thingers(ticket_count, rewards):
     tc = ticket_count.copy()
     
     for key in tc.keys():
         intkey = int(key)
         for i in range(1, tc[key] + 1):
-            for i in range(1, rw[key] + 1):
+            for i in range(1, rewards[key] + 1):
                 tc[str(intkey + i)] += 1
     
     return tc 
             
 def check_card(card):
     found_numbers = []
-    index_1 = card.find(':')
-    game = card[:index_1].strip('Card ')
+    i1 = card.find(':')
+    i2 = card.find('|')
 
-    card = card[index_1+1:]
-    index_1 = card.find('|')
-    numbers_on_card = card[:index_1].strip().split(' ')
-    scratched_numbers = card[index_1 + 1:].strip()
-    scratched_numbers = re.sub(' +', ' ', scratched_numbers).split(' ')
 
-    for scratch in scratched_numbers:
-        if scratch in numbers_on_card:
-            found_numbers.append(scratch)
+    numbers = card[i1+1:i2].split()
+    scratch = card[i2+1:].split()
+    game = card[:i1].strip('Card ')
+
+    for itch in scratch:
+        if itch in numbers:
+            found_numbers.append(itch)
 
     return game, len(found_numbers)
 
