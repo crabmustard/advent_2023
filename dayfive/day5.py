127278f9f8cf36788fd9e05d13a411b8d4d53552
crabmustard <bryce.lippert@gmail.com>
2023-12-05 19:06:38 -0500

    broke something. might fix one day

--- dayfive/day5.py
+++ dayfive/day5.py
@@ -19,22 +19,7 @@ def process_almanac(nac):
     p_temp = process_next(temp)
     p_hum = process_next(hum)
     p_loc = process_next(loc)
-    return [p_seeds, p_soil, p_fert, p_water, p_light, p_temp, p_hum, p_loc]
-    
-
-def process_almanac_two(nac):
-    new_a = list(filter(lambda x: len(x) > 0, nac.split('\n\n')))
-    seeds, soil, fert, water, light, temp, hum, loc = new_a
-    p_seeds = process_seeds(seeds)
-    m_seeds = pair_seeds(p_seeds)
-    p_soil = process_next(soil)
-    p_fert = process_next(fert)
-    p_water = process_next(water)
-    p_light = process_next(light)
-    p_temp = process_next(temp)
-    p_hum = process_next(hum)
-    p_loc = process_next(loc)
-    return [m_seeds, p_soil, p_fert, p_water, p_light, p_temp, p_hum, p_loc]
+    return p_loc
     
 
 
@@ -45,11 +30,6 @@ def process_seeds(seedline):
     seed_list = [int(i) for i in seed_list] 
     return seed_list
 
-def pair_seeds(seed_list):
-    seed_pairs = []
-    for i in range(0, len(seed_list), 2):
-        seed_pairs.append([seed_list[i],seed_list[i+1]])
-    return seed_pairs
 
 def process_next(this_map):
     result_ranges = []
@@ -66,13 +46,12 @@ def process_next(this_map):
 alm = make_nac('day5test.txt')
 
 maybe = process_almanac(alm)
-pprint(maybe)
+print(maybe)
 
 
 
 
 
-# def process_seeds(seeds, almanac)
 
 
 
