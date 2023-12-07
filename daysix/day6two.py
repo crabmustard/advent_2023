b8bda74f1ad46900f1f8f721164625051a36533c
crabmustard <bryce.lippert@gmail.com>
2023-12-06 19:39:05 -0500

    i dont know at this point what im doing

--- daysix/day6two.py
+++ daysix/day6two.py
@@ -5,29 +5,6 @@ race2 = 55826490
 dist2 = 246144110121111
 
 
-
-# improper loop, doesn't fully break ~.5 seconds
-
-
-# def find_possible(time, record):
-#     first = int(record/time)
-#     last = time - first * 2
-#     min_time = 0
-#     max_time = 0
-#     for mi in range(first, first * 2):
-#         trav = mi * (time - mi)
-#         if trav > record:
-#             min_time = mi
-#             break
-#     for ma in range(last, time-first):
-#         trav = ma * (time - ma)
-#         if trav < record:
-#             max_time = ma
-#             break
-#     return (max_time - min_time)
-
-
-
 def run_race(time, record):
     first = int(record/time)
     last = time - first * 2
