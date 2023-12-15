645384665bcde15c962a71c8e525e792526ad056
crabmustard <bryce.lippert@gmail.com>
2023-12-15 02:00:01 -0500

    .py version

---
+++ dayfifteen/day15.py
@@ -0,0 +1,43 @@
+from collections import OrderedDict
+
+def hash_seg(segment):
+    hsh = 0
+    for i in segment:
+        hsh += ord(i)
+        hsh *= 17
+        hsh %= 256
+    return hsh
+
+
+code = open('day15.txt').read()
+split = code.split(',')
+
+main_dict = OrderedDict()
+for line in split:
+    if '=' in line:
+        key = line[:-2]
+        hsh = str(hash_seg(key))
+        val = line[-1]
+        if hsh not in main_dict:
+            main_dict[hsh] = OrderedDict()
+        if key not in main_dict[hsh]:
+            main_dict[hsh][key] = -1
+        main_dict[hsh][key] = val
+    elif '-' in line:
+        key = line[:-1]
+        hsh = str(hash_seg(key))
+        if hsh in main_dict:
+            if key in main_dict[hsh]:
+                main_dict[hsh].pop(key)
+
+z = []
+for hsh in main_dict.keys():
+    cnt = 1
+    for key in main_dict[hsh].keys():
+        z.append([int(hsh)+1, int(main_dict[hsh][key]), cnt])
+        cnt += 1
+
+total = 0
+for i in z:
+    total += i[0] * i[1] * i[2]
+print(total)
